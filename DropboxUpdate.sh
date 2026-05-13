e77ad0d71c924ffc8327886205899ace4ab45459
Hilary Weller <h.weller@reading.ac.uk>
2026-05-13 15:55:10 +0100

    Changes after self proof reading, including more info on exercises.

--- DropboxUpdate.sh
+++ DropboxUpdate.sh
@@ -1,3 +1,6 @@
 cp -u metNumerics.tex /home/hilary/Dropbox/MFC_book/climate_num.tex
 cp -ru climate_numFigures /home/hilary/Dropbox/MFC_book
 dropboxd &
+
+#lyx --export pdf -f all main.lyx
+cp main.pdf /home/hilary/Dropbox/MFC_book/climate_numFigures/HilaryMetNumerics.pdf
