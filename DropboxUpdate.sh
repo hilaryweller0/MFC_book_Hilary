2dc546fc14b225560c9d4b296d45c57416685fc9
Hilary Weller <h.weller@reading.ac.uk>
2026-05-15 16:15:56 +0100

    A few lines in the ML section, and a few lines on DA in the abstract

--- DropboxUpdate.sh
+++ DropboxUpdate.sh
@@ -1,6 +1,11 @@
-cp -u metNumerics.tex /home/hilary/Dropbox/MFC_book/climate_num.tex
-cp -ru climate_numFigures /home/hilary/Dropbox/MFC_book
+lyx --export pdflatex -f all main.lyx; \
+    pdflatex main.tex; bibtex main; pdflatex main; pdflatex main
+rmtexall main.tex
+cp main.pdf /home/hilary/Dropbox/MFC_book/climate_numFigures/HilaryMetNumerics.pdf
+
 dropboxd &
 
-#lyx --export pdf -f all main.lyx
-cp main.pdf /home/hilary/Dropbox/MFC_book/climate_numFigures/HilaryMetNumerics.pdf
+#cp -u metNumerics.tex /home/hilary/Dropbox/MFC_book/climate_num.tex
+meld metNumerics.tex /home/hilary/Dropbox/MFC_book/climate_num.tex &
+cp -ru climate_numFigures /home/hilary/Dropbox/MFC_book
+
